32419128d57cd4027355b21b508b5aaa31d2299e
Andrew Gonzales <andrewmartingonzales@gmail.com>
2015-09-21 15:15:55 -0700

    Render test marker in map center

--- client/app/app.js
+++ client/app/app.js
@@ -3,4 +3,9 @@ var bikeApp = angular.module('bikeApp', ['uiGmapgoogle-maps']);
 
 bikeApp.controller('BikeController', function($scope){
   $scope.map = {center: {latitude: 37.779, longitude: -122.419}, zoom: 14}; //sf city hall to start
+  $scope.testMarker = {
+    id: 0,
+    coords: {latitude: 37.779, longitude: -122.419},
+    click: console.log('clicked a marker')
+  };
 });
